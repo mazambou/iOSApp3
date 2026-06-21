756c584680bc35e701cdd10c99d69d0b5206fb8d
AZAMBOU Marcel <azambou@mac.home>
2026-06-20 22:12:09 -0400

    Initial project setup

---
+++ iOSApp3/iOSApp3/iOSApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp3App.swift
+//  iOSApp3
+//
+//  Created by AZAMBOU Marcel on 2026-06-19.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
